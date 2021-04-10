669f90b0f2198d16a0f17b6dd5c3fe1f66b55a99
Linh <linhgraphic@gmail.com>
2021-04-10 13:05:07 +0100

    git ignore config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SECRET_KEY: "top secret info1234",
+  SECRET_KEY: "renew secret key Linh1234",
   Url:
     "mongodb+srv://linhNP:Mongodb_linh86@mylib.guzx4.mongodb.net/my-library?retryWrites=true&w=majority",
 };
